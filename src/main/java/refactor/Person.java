676705e7e301152c5312adc095b4d96de69fce1c
andrey.kuznetsov <andrey198701@gmail.com>
2015-09-28 19:51:35 +0300

    initial commit

---
+++ src/main/java/refactor/Person.java
@@ -0,0 +1,29 @@
+package refactor;
+import java.util.Date;
+
+public class Person {
+	private String NAME;
+	private PhoneNumber phoneNumber;
+	private Date date;
+	
+	public Person(String name, PhoneNumber phoneNumber) {
+		this.NAME = name;
+	}
+
+	public String getName() {
+		return NAME;
+	}
+
+	public void setName(String name) {
+		name = name;
+	}
+
+	public PhoneNumber getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(PhoneNumber phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
